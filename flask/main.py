b5be0852b2debe41d048a812e377fc50dafaf415
e2102793 <joona114@hotmail.fi>
2023-03-21 22:06:33 +0200

    forecast route added

--- flask/main.py
+++ flask/main.py
@@ -3,25 +3,45 @@ import os
 import requests
 from flask_cors import CORS
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
+
 API_KEY = os.environ["API_KEY"]
 BASE_URL = "https://api.openweathermap.org/data/2.5"
-LAT = 63.09525
-LNG = 21.61627
-CITY = "Vaasa"
+LAT = 60.192059
+LNG = 24.945831
 
 
-@app.route("/")
-def hello():
-    return "<p>Hello world</p>"
+@app.route("/api/location", methods=["POST"])
+def receive_location():
+    data = request.json
+    latitude = data.get("latitude")
+    longitude = data.get("longitude")
+    global LAT, LNG
+    LAT = latitude
+    LNG = longitude
+    return "Location received"
 
 
 @app.route("/current")
 def current():
     r = requests.get(
         f"{BASE_URL}/weather?&lat={LAT}&lon={LNG}&appid={API_KEY}&units=metric"
+    )
+    response = r.json()
+    icon = response["weather"][0]["icon"]
+    icon_url = f"http://openweathermap.org/img/w/{icon}.png"
+    icon = {"icon": icon_url}
+    response.update(icon)
+
+    return response
+
+
+@app.route("/forecast")
+def forecast():
+    r = requests.get(
+        f"{BASE_URL}/forecast?lat={LAT}&lon={LNG}&appid={API_KEY}&units=metric"
     )
     return r.json()
